bced0e72f0ec2b7817bef1e7c04edbf5e7c41f7b
Lakshay Bansal <lakshayb057@gmail.com>
2025-11-03 11:41:15 +0530

    Updated code with latest fixes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
-const API = axios.create({
+const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'https://nexuscrm.onrender.com/api',
   withCredentials: true
 });
